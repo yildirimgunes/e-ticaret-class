ee0dccb6a86c52fb57228cba975c2dd6f0e102b4
yildirimgunes <yildirimgunes1973@gmail.com>
2023-04-12 23:12:56 +0300

    12.04.2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+//import Footer from './components/footer/Footer'
+//import Header from './components/header/Header'
+import {Header, Footer} from "./components"
+//import Contact from './pages/contact/Contact'
+//import Home from './pages/home/Home'
+import {Contact, Home} from "./pages"
 
 
 const App = () => {
   return (
-    <div>App
+    <>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
 
-    </div>
+    </>
   )
 }
 
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,6 +4,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>Contact</div>
+
   )
 }
 
