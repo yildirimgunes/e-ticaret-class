1bb331e8d7c4f68ba64655cbcde69f100c5de264
yildirimgunes <yildirimgunes1973@gmail.com>
2023-04-12 10:04:31 +0300

    e-ticaret baslangic formatı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {BrowserRouter, Route, Routes from react-router-dom}
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+
+      </BrowserRouter>
+    </>
   )
 }
 
