466745b6e78a5fe7ceb840620bcdf44e69779b84
yildirimgunes <yildirimgunes1973@gmail.com>
2023-04-13 23:36:42 +0300

    13.04.2023

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,67 @@
 //// tüm linklerin olduğu başlık kısmı
+//import { DomPlatform } from 'chart.js';
 import React from 'react'
+import styles from "./Header.module.scss";
+import {Link} from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa"
+import { HiOutlineMenuAlt3 } from "react-icons/hi"
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
+  const [showMenu, setShowMenu]=useState(False);
+  const logo = (
+    <div className={styles.logo}>
+      <Link to="/">
+        <h2>
+          e<span>Shop</span>.
+        </h2>
+      </Link>
+
+    </div>
   )
+    const cart= (
+      <span className={styles.cart}>
+        <Link to="/cart">
+          Cart
+          <FaShoppingCart size={20}/>
+          <p>0</p>
+        </Link>
+
+      </span>
+
+    )
+  return (
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]}>
+            <span clasName={styles.links}>
+              <Link to="/login">Login</
+              Link>
+              <Link to="/order-history">My Orders</
+              Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+        <div classsName={styles["menu-icon"]}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28}/>
+        </div>
+      </div>
+    </header>
+
+  )
+
 }
 
 export default Header
