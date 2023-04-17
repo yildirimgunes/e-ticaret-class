faf2456ec35ee39f0ac831abc1bdec10e13cd87d
yildirimgunes <yildirimgunes1973@gmail.com>
2023-04-17 05:01:32 +0300

    17.04.2023

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,18 +1,28 @@
 //// kayıtlı olmayan kulllanıcılar için kayıt sayfası
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './auth.module.scss'
 import registerimg from '../../assets/register.png'
 import Card from '../../components/card/Card'
 import { Link} from 'react-router-dom'
 import { toast } from "react-toastify"
 
+const [email,setEmail]= useState("")
+    const [password, setPassword]= useState("")
+    const [cPassword, setCPassword]= useState("")
+
+
 const Register = () => {
+  const [email,setEmail]= useState("")
+  const [password, setPassword]= useState("")
+  const [cPassword, setCPassword]= useState("")
+
+  const registerUser= (e) => {e.preventDefault(); console.log(email, password, cPassword) }
   return (
     <>
     <section className={`container ${styles.auth}`}>
       <Card cardClass={styles.form}>
         <h2>Register</h2>
-        <form >
+        <form "onSubmit">
           <input type="text" placeholder="Email" required/>
           <input type="password" placeholder="Password" required/>
           <input type="password" placeholder="Confirm Password" required/>
