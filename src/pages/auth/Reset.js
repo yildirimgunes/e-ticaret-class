6cb19fc518898da01f2049f78ed0642feccdc43e
yildirimgunes <yildirimgunes1973@gmail.com>
2023-05-12 20:00:03 +0300

    12.05.2023

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,11 +1,19 @@
 ////şifre resetleme sayfası
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.scss"
 import resetImg from '../../assets/forgot.png'
 import Card from '../../components/card/Card'
 import { Link } from 'react-router-dom'
 
 const Reset = () => {
+
+  const [email, setEmail]=useState("")
+
+  const resetPassword= (e) => {
+    e.preventDefault();
+    alert(email)
+
+  }
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
@@ -13,8 +21,9 @@ const Reset = () => {
       </div>
       <Card cardClass={styles.form}>
         <h2>Reset</h2>
-        <form>
-          <input type="text" placeholder="Email" required/>
+        <form onSubmit={resetPassword}>
+          <input type="text" placeholder="Email" required value=
+          {email}/>
           <buton className={"--btn --btn-primary --btn-block"}>Reset Password</buton>
           <div className={styles.links}>
             <p>
