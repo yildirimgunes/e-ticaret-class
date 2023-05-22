9b5aca207643255afa633751e23e4752600c5cc9
yildirimgunes <yildirimgunes1973@gmail.com>
2023-05-22 15:40:04 +0300

    22.05.2023

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,26 @@
 //// admin panelindeki allproducts componenti
-import React from 'react'
+import React, {useState} from 'react'
 
 const ViewProducts = () => {
+
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+
+  }, [])
+
+  
+  const getProducts = () => {
+    setIsLoading(true)
+    try {
+      const productRef = collection (db, "products")
+    }
+    catch(error){
+      setIsLoading (false)
+    }
+      toast.error(error.message)
+  }
   return (
     <div>ViewProducts</div>
   )
