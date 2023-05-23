d483e43d0809718f99a83ea550d75f2657a49da3
yildirimgunes <yildirimgunes1973@gmail.com>
2023-05-23 14:15:17 +0300

    23.05.2023

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,11 +1,15 @@
 //// admin panelindeki allproducts componenti
-import {collection,onSnapshot,orderBy, query} from 'firebase/firestore'
+import {collection,deleteDoc,doc,onSnapshot,orderBy, query} from 'firebase/firestore'
 import React, {useEffect, useState} from 'react'
 import { toast } from 'react-toastify'
-import { db } from '../../../firebase/config'
+import { db, storage } from '../../../firebase/config'
 import Loader from '../../loader/Loader'
 import styles from './ViewProducts.module.scss'
 import { FaTrashAlt, FaEdit } from "react-icons/fa"
+import { Link } from 'react-router-dom'
+import { deleteObject, ref } from 'firebase/storage'
+import Notiflix from 'notiflix'
+
 
 const ViewProducts = () => {
 
@@ -13,7 +17,7 @@ const ViewProducts = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-
+    getProducts();
   }, [])
 
   
@@ -36,8 +40,41 @@ const ViewProducts = () => {
     }
     catch(error){
       setIsLoading (false)
-    }
       toast.error(error.message)
+    }
+      
+  }
+
+  const confirmDelete=(id, imageURL) => {
+    Notiflix.Confirm.show (
+      "Delete Products",
+      "You are about to delete this product",
+      "Delete",
+      "Cancel",
+      function okCb(){
+        deleteProduct(id, imageURL)
+      },
+      function cancelCb(){
+
+      },
+      {
+        width:"320",
+        borderRadius:"3px",
+        titleColor:"orangered",
+        okButtonBackground: "orangered"
+        cssAnimationStyle:"zoom"
+      }
+    )
+  }
+  const deleteProduct= async(id, imageURL)=> {
+    try{
+      await deleteDoc(doc(db, "products","id"))
+      const storageRef=ref(storage, imageURL)
+      await deleteObject(storageRef)
+      toast.success("Product deleted successfully")
+    } catch (error){
+      toast.error(error.message)
+    }
   }
   return (
     <>
@@ -66,7 +103,7 @@ const ViewProducts = () => {
                   <td>
                     {index + 1}
                   </td>
-                    <img src={imageURL} alt={name} style={{"100px"}}></img>
+                    <img src={imageURL} alt={name} style={{width:"100px"}}></img>
                   <td>
                     {name}
                   </td>
@@ -75,11 +112,11 @@ const ViewProducts = () => {
                     {`$${price}`}
                   </td>
                   <td classname={styles.icons}>
-                    <Link to="/admin/add-product"
+                    <Link to={`/admin/add-product/${id}`}>
                       <FaEdit size={20} color="green"/>
                     </Link>
                     &nbsp;
-                    <FaTrashAlt size={18} color="red"/>
+                    <FaTrashAlt size={18} color="red" onClick={() => confirmDelete(id, imageURL)}/>
                   </td>
                 </tr>
               )
